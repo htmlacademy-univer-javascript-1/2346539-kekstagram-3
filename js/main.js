7b9007337f148e21c0de92aa781efcb7e4392788
Mihail <mihanik03@gmail.com>
2023-05-11 23:23:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-function getRandomInt(from, to) {
-    return Math.floor(from + Math.random() * (to - from));
-  }
-
-  function checkCommentLength(string, Length) {
-    if(string.length <= Length) {
-      return true;
-    } else {
-      return false;
-    }
+function getRandomInt(min, max) {
+    return Math.floor(min + Math.random() * (max - min));
   }
 
   const button = document.querySelector('.img-upload__label');
   button.onclick = function() {
-    getRandomInt(1, 10);
+    getRandomInt(1, 100);
     checkCommentLength('sdgs', 3);
+    generateContent(25);
   };
+
+  function generateContent(size) {
+    const content = [];
+    for(let i = 0; i < size - 1; i++) {
+      content[i] = generateObj(i + 1);
+    }
+    return content;
+  }
+
+  function generateObj(id) {
+    const newObj = {
+      id: id,
+      url: `photos/${id}.jpg`,
+      description: '',
+      likes: getRandomInt(15, 200),
+      comments: getRandomInt(0, 200)
+    };
+    return newObj;
+  }
