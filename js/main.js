e2e7f7d7269eb89a3007f38bbf3162f04b4af3e0
Mihail <mihanik03@gmail.com>
2023-05-11 23:46:07 +0300

    3

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-function getRandomInt(from, to) {
-    return Math.floor(from + Math.random() * (to - from));
-  }
+function getRandomInt(min, max) {
+  return Math.floor(min + Math.random() * (max - min));
+}
 
-  function checkCommentLength(string, Length) {
-    if(string.length <= Length) {
-      return true;
-    } else {
-      return false;
-    }
+function checkCommentLength(string, requiredLength) {
+  if(string.length <= requiredLength) {
+    return true;
+  } else {
+    return false;
   }
+}
 
-  const button = document.querySelector('.img-upload__label');
-  button.onclick = function() {
-    getRandomInt(1, 10);
-    checkCommentLength('sdgs', 3);
-  };
+const button = document.querySelector('.img-upload__label');
+button.onclick = function() {
+  getRandomInt(1, 100);
+  checkCommentLength('sdgs', 3);
+};
